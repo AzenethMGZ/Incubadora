7448b15fbf45d000a51058ca6d81e32ec4ce7833
jared <salazarloerajared@gmail.com>
2022-12-13 11:45:05 -0600

    Jared v0.0.1

--- app/src/main/java/com/example/incubadora/sensores.java
+++ app/src/main/java/com/example/incubadora/sensores.java
@@ -3,6 +3,10 @@ package com.example.incubadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class sensores extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class sensores extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensores);
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Toast.makeText(sensores.this, "timer", Toast.LENGTH_SHORT).show();
+            }
+        };
+        timer.schedule(task,1000);
     }
+
 }
